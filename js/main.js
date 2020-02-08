557f194e9096a81d715547eafbe8f341c2cd3b03
AntonioMarcosMLeite <amml01@yahoo.com.br>
2020-02-08 18:56:02 -0300

    Criação da index.html

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/*var nome = "Marcos";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+alert(nome + " tem" + n1 + " anos.");
+alert(n1 + n2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.tolowerCase());
+console.log(frase.replace("Japão", "Brasil"));
+
+alert ("Seja Bem Vindo " + nome);
+
+
+var lista = ["maça", "pêra", "laranja"];
+lista.push("uva");
+lista.pop();
+
+
+var fruta = {nome="maça", cor="vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+
+var frutas = [{nome="maça", cor="vermelha"}, {nome="uva", cor="roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+
+
